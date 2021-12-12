34da9b4cb009987d0ae2764c535d5bd4373d445c
DarkKnight20 <95390503+DarkKnight20@users.noreply.github.com>
2021-12-12 12:37:57 -0500

    Add files via upload

---
+++ Basic Data Types Activity/WorkingWithBasicDataTypesAssignment.js
@@ -0,0 +1,32 @@
+// Four-integer variables
+let opening = "Before I get started, let me say this on the outset. Welcome to my world.";
+let name = "Adrian Jeff Jackson";
+let title = "Bounty Hunter";
+let worldName = "Mars";
+
+// Three-integer variables
+let age = 16;
+let year = 2789;
+let century = 28;
+
+// One-array variable
+let familyMembers = ["Jacob", "Sarah", "Chase", "Nick"];
+
+// One-boolean variable
+let hasParents = false;
+
+console.log(opening + " My name is " + name + ". I'm a " + title + " in the year " + year + ".");
+console.log("\nYes, you read that right. It is " + year + ", the " + century + "th century and guess what? Reality here, it sucks.");
+console.log("\nI may have forgotten to tell you this but I live on " + worldName + " and so does practically all of humanity. Or rather, what's left of it. Allow me to explain everything.");
+console.log("\nA long time ago, humanity decided that we couldn't live on the Earth anymore. You know, because of stuff like Climate Change, Global Warming, all that beautiful crap.");
+console.log("Eventually, we sent men to " + worldName + " and in a few years, we completely colonised it with a few astronauts and even a few civilians from Earth");
+console.log("However, what we didn't know is that someone, or rather something was waiting for us on " + worldName + ": aliens.");
+console.log("\nWe call them the Black Martians now, they're big hulklike creatures completely covered in black skin.");
+console.log("\nLong story short, they invaded our world. Killed half of us, enslaved the other half on" + worldName + "and we've been enslaved here ever since.");
+console.log("Not everyone here is a slave. Some people manage to escape and become " + title + "s, like me. I'm " + age + " years old. I became a bounty hunter when I was just 13 years old.");
+console.log("In case you're wondering what we do, " + title + "s hunt down Black Martians, kill them and sell them and their inventory through the underground black market and make money.");
+console.log("I know what some of you might be thinking, 'Your parents are good with this?' Yeah, " + hasParents + ", I don't have parents.");
+console.log("\nMy brother, " + familyMembers[0] + ", and I never knew our biological parents. We grew up our entire lives looking after one another, we only had ourselves and our friends: " + familyMembers[1] + ", " + familyMembers[2] + " and " + familyMembers[3]);
+console.log("I've done some math and whoever is reading this note should be from the year 2021. Look, buddy, whoever you are, I need you to do something for me. I have a mission for you: don't let humanity go to " + worldName + ". Ever.");
+console.log("\nDo whatever it takes, I don't care, just don't let us do it. Stop humanity from going to " + worldName + ". Don't let us do it, if you stop us, you'll be saving humanity. I know this is a lot to ask, but believe me, it's worth it");
+console.log("I'll try and provide you with more evidence of this note and more information. I'll write another note to you as soon as I can, if I'm still alive that is.");
